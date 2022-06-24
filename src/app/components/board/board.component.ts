0b306971fe74712cc189600e64dd8652c955f15b
JGhignatti <joao.ghignatti@inf.ufrgs.br>
2022-06-24 03:16:42 -0300

    Style app root and create board component

---
+++ src/app/components/board/board.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jv-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss'],
+})
+export class BoardComponent {
+}
