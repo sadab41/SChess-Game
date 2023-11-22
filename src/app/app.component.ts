e3b3ab08f66ea23439bf0ac7c2233aee65ff20ba
sadabilyas5 <sadabilyas5@gmail.com>
2023-11-22 10:39:00 +0530

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'jv-root',
+  selector: 'sadab-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
